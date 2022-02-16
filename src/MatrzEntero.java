f06c3925b3cfd5c26cf1d451b5617031e56e6ab8
isidro <ffabianiant12@gmail.com>
2022-02-15 18:23:34 -0700

    Inicializacion del proyecto

---
+++ src/MatrzEntero.java
@@ -0,0 +1,27 @@
+/*
+ Diseña un TDA que permita construir objetos que almacenen una matriz CUADRADA de enteros. El constructor de la clase recibirá un ENTERO indicando el tamaño de RENGLONES Y COLUMNAS que se deberá construir. Las operaciones y comportamiento es el siguiente:
+
+- sumarValores: éste método no tiene parámetro, y obtiene la suma de TODOS LOS VALORES capturados de la matriz, retornando la suma de los mismos.
+
+- sumarValoresRenglon(int numRenglon): éste método regresa la suma de valores del renglón indicado por la variable 
+
+- sumarValoresColumna(int numColumna): igual que el anterior pero para columnas, indicando por la variable numColumna el índice de columa que se desea sumar.
+
+- intercambioRenglones(int reng1, int ren2): Realiza un intercambio de datos entre el renglon 1 y renglon 2.
+
+- intercambioColumnas(int col1, int col2): Realiza un intercambio de valores entre las columnas 1 y 2.
+
+- copiarRenglon(int origen, int destino): Realiza un copiado de valores desde renglon origen hacia renglón destino.
+
+- copiarColumna(int colOrigen, int ColDestino): Copia los datos de la columna origen sobre la destino.
+
+- inversa: no lleva parámetro, retorna la matriz inversa, es decirlo los renglones los vuelve columnas y viceversa.
+ */
+
+/**
+ *
+ * @author IsidroAntonio
+ */
+public class MatrzEntero {
+    
+}
